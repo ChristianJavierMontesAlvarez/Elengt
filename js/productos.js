bef32e655d17dc057ff180635ee20d547f6ce85e
Christian Alvarez <christian.987.cma@gmail.com>
2023-01-31 22:57:42 -0300

    version_1.3

--- js/productos.js
+++ js/productos.js
@@ -5,7 +5,7 @@
 const productos_hombre = [
     {id: 1, nombre: "Traje Completo Azul", color: "Azul", tipo: "traje completo", genero: "masculino", descripción: "Traje para Hombre Color Azul elegante de excelente calidad.", precio: 50000, img1: "hombre_traje_completo.png", img2: "hombre_traje_completo2.png", ofertas: true, nuevo: false, s: 10, m: 10, l: 5},
     {id: 2, nombre: "Traje Completo Gris", color: "gris", tipo: "traje completo", genero: "masculino", descripción: "Traje gris para hombre elegante", precio: 45000, img1: "hombre_traje_gris.png", img2: "hombre_traje_gris2.png", ofertas: true, nuevo: false, s: 10, m: 10, l: 5},
-    {id: 3, nombre: "Chaqueta Elegante Hombre", color: "negro", tipo: "chaqueta", genero: "masculino", descripción: "Chaqueta para hombre con apariencia formal", precio: 5500, img1: "chaqueta_negro_hombre.png", img2: "none", ofertas: false, nuevo: true, s: 10, m: 10, l: 5},
+    {id: 3, nombre: "Chaqueta Elegante Hombre", color: "negro", tipo: "chaqueta", genero: "masculino", descripción: "Chaqueta para hombre con apariencia formal", precio: 5500, img1: "https://github.com/ChristianJavierMontesAlvarez/Elengt/tree/main/img/chaqueta_negro_hombre.png", img2: "none", ofertas: false, nuevo: true, s: 10, m: 10, l: 5},
     {id: 4, nombre: "Pantalon Negro Hombre", color: "negro", tipo: "pantalon", genero: "masculino", descripción: "Pantalon formal para hombre para esos momentos de formalidad", precio: 5000, img1: "pantalon_hombre_negro.png", img2: "none", ofertas: false, nuevo: true, s: 10, m: 10, l: 5},
 ];
 
